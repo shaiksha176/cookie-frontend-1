006aae86ccae1c4fddecec52bf5a71139e36f6f5
shaiksha176 <iamshaiksha17@gmail.com>
2025-01-01 22:48:05 +0530

    first commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/";
+const API_BASE_URL = "http://23.21.66.149:5000/api/";
 
 const api = {
   register: async (userData) => {
